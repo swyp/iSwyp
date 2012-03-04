2c1e2b8a63ccbd638e963e9d0375b774b54c418e
Alexander List <alist@listprojects.com>
2012-03-04 12:59:54 -0500

    show swyp content selectors as popovers now

--- swyp/ISUnifiedSwypSpace.h
+++ swyp/ISUnifiedSwypSpace.h
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 #import "ISSwypHistoryItem.h"
+#import "ISPasteboardVC.h"
+#import "ISCalendarVC.h"
+#import "ISContactVC.h"
+#import "ISHistoryScrollVC.h"
 
-
-@interface ISUnifiedSwypSpace : UIViewController <swypConnectionSessionDataDelegate, swypContentDataSourceProtocol>{
-	
-	NSManagedObjectContext *		_objectContext;
-	swypWorkspaceViewController *	_swypWorkspace;
+@interface ISUnifiedSwypSpace : UIViewController <swypConnectionSessionDataDelegate, swypContentDataSourceProtocol, UINavigationControllerDelegate, UIImagePickerControllerDelegate>{
 	
 	UIButton *						_photoButton;
 	UIButton *						_contactButton;
@@ -24,10 +24,21 @@
 }
 @property (nonatomic, assign) id<swypContentDataSourceDelegate>	datasourceDelegate;
 @property (nonatomic, strong) NSManagedObjectContext *			objectContext;
+@property (nonatomic, strong) swypWorkspaceViewController * swypWorkspace;
 @property (nonatomic, strong) swypWorkspaceView *				workspaceView;
 @property (nonatomic, strong) NSMutableDictionary *				contentThumbnailForPendingFilesBySession;
 
+@property (nonatomic, strong) ISContactVC *					contactVC;
+@property (nonatomic, strong) ISCalendarVC *				calendarVC;
+@property (nonatomic, strong) UIImagePickerController *		imageVC;
+@property (nonatomic, strong) UIImagePickerController *		cameraVC;
+@property (nonatomic, strong) UIPopoverController *			displayPopoper;
+
+@property (nonatomic, strong) ISHistoryScrollVC *			historyVC;
 
 -(id) initWithObjectContext:(NSManagedObjectContext*)context swypWorkspace:(swypWorkspaceViewController*)workspace;
 
+
+///convinience encapsulating popovers and vcs
+-(void) displayVC:(UIViewController*)vc fromRect:(CGRect)rect fromVC:(UIViewController*)displayController;
 @end
--- swyp/ISUnifiedSwypSpace.m
+++ swyp/ISUnifiedSwypSpace.m
@@ -9,7 +9,12 @@
 #import "ISUnifiedSwypSpace.h"
 
 @implementation ISUnifiedSwypSpace
-@synthesize datasourceDelegate = _datasourceDelegate, objectContext = _objectContext, contentThumbnailForPendingFilesBySession, workspaceView = _workspaceView;
+@synthesize datasourceDelegate = _datasourceDelegate, objectContext = _objectContext, contentThumbnailForPendingFilesBySession, swypWorkspace = _swypWorkspace, workspaceView = _workspaceView;
+@synthesize   contactVC = _contactVC, calendarVC = _calendarVC, imageVC = _imageVC, cameraVC = _cameraVC;
+@synthesize displayPopoper = _displayPopoper;
+@synthesize historyVC = _historyVC;
+
+
 
 -(id) initWithObjectContext:(NSManagedObjectContext*)context swypWorkspace:(swypWorkspaceViewController*)workspace{
 	if (self  = [super initWithNibName:nil bundle:nil]){
@@ -50,20 +55,23 @@
 	double	buttonHeight	=	65;
 	_photoButton	=	[UIButton buttonWithType:UIButtonTypeRoundedRect];
 	[_photoButton setFrame:CGRectMake(0, 0, round(_buttonTray.width / 3), buttonHeight)];
-	[_photoButton setTitle:LocStr(@"Camera",@"Camera workspace button") forState:UIControlStateNormal];
+	[_photoButton setTitle:LocStr(@"Photos",@"photos workspace button") forState:UIControlStateNormal];
 	[_photoButton setAutoresizingMask:UIViewAutoresizingFlexibleWidth| UIViewAutoresizingFlexibleRightMargin];
+	[_photoButton addTarget:self action:@selector(pressedPhotoButton:) forControlEvents:UIControlEventTouchUpInside];
 	[_buttonTray addSubview:_photoButton];
 
 	_calendarButton	=	[UIButton buttonWithType:UIButtonTypeRoundedRect];
 	[_calendarButton setFrame:CGRectMake(round(_buttonTray.width / 3), 0, round(_buttonTray.width / 3), buttonHeight)];
 	[_calendarButton setTitle:LocStr(@"Calendar",@"Calendar workspace button") forState:UIControlStateNormal];
 	[_calendarButton setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin];
+	[_calendarButton addTarget:self action:@selector(pressedCalendarButton:) forControlEvents:UIControlEventTouchUpInside];
 	[_buttonTray addSubview:_calendarButton];
 
 	_contactButton	=	[UIButton buttonWithType:UIButtonTypeRoundedRect];
 	[_contactButton setFrame:CGRectMake(2*round(_buttonTray.width / 3), 0, round(_buttonTray.width / 3), buttonHeight)];
 	[_contactButton setTitle:LocStr(@"Contact",@"Contact workspace button") forState:UIControlStateNormal];
 	[_contactButton setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleLeftMargin];
+	[_contactButton addTarget:self action:@selector(pressedContactButton:) forControlEvents:UIControlEventTouchUpInside];
 	[_buttonTray addSubview:_contactButton];
 	
 	
@@ -72,6 +80,7 @@
 	[_historyButton setFrame:CGRectMake(0, heightOffset, self.view.width, _buttonTray.size.height - heightOffset)];
 	[_historyButton setTitle:LocStr(@"Received Stuff",@"received items workspace button") forState:UIControlStateNormal];
 	[_historyButton setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
+	[_historyButton addTarget:self action:@selector(pressedHistoryButton:) forControlEvents:UIControlEventTouchUpInside];
 	[_buttonTray addSubview:_historyButton];
 	
 	[self.view addSubview:_buttonTray];
@@ -87,7 +96,78 @@
 	
 }
 
+#pragma mark button response
+-(void) pressedPhotoButton:(UIButton*)sender{
+	if (_imageVC == nil){
+		self.imageVC			=	[[UIImagePickerController alloc] init];
+		_imageVC.allowsEditing	=	NO;
+		[_imageVC setDelegate:self];
+	}
+	
+	CGRect dispRect		= [sender convertRect:sender.frame toView:self.view];
+	dispRect.origin.x	=	sender.origin.x;
+	[self displayVC:self.imageVC fromRect:dispRect fromVC:self];
 
+	//gotta call this after display
+	if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+		[[[_imageVC navigationBar] topItem] setLeftBarButtonItem:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(pressedCameraButton:)]];
+	}
+}
+
+-(void) pressedCameraButton:(UIBarButtonItem*)sender{
+	if (_cameraVC == nil){
+		self.cameraVC			=	[[UIImagePickerController alloc] init];
+		_cameraVC.allowsEditing	=	NO;
+		[_cameraVC setSourceType:UIImagePickerControllerSourceTypeCamera];
+		[_cameraVC setShowsCameraControls:TRUE];
+		[_cameraVC setDelegate:self];
+	}
+	
+	CGRect dispRect		= [_photoButton convertRect:_photoButton.frame toView:self.view];
+	dispRect.origin.x	= _photoButton.origin.x;
+	[self displayVC:self.cameraVC fromRect:dispRect fromVC:self];
+}
+
+-(void) pressedContactButton:(UIButton*) sender{
+	if (_contactVC == nil){
+		self.contactVC =	[[ISContactVC alloc] init];
+	}
+	
+	CGRect dispRect		= [sender convertRect:sender.frame toView:self.view];
+	dispRect.origin.x	=	sender.origin.x;
+	[self displayVC:self.contactVC fromRect:dispRect fromVC:self];
+	
+}
+-(void) pressedCalendarButton:(UIButton*) sender{
+	if (_calendarVC == nil){
+		self.calendarVC =	[[ISCalendarVC alloc] init];
+	}
+	
+	CGRect dispRect		= [sender convertRect:sender.frame toView:self.view];
+	dispRect.origin.x	=	sender.origin.x;
+	[self displayVC:self.calendarVC fromRect:dispRect fromVC:self];
+}
+-(void) pressedHistoryButton:(UIButton*)sender{
+	if (_historyVC == nil){
+		self.historyVC = [[ISHistoryScrollVC alloc] initWithObjectContext:self.objectContext swypWorkspace:self.swypWorkspace];
+	}
+	[self displayVC:self.historyVC fromRect:[sender convertRect:sender.frame toView:self.view] fromVC:self];
+}
+
+#pragma mark specialty viewC
+-(void) displayVC:(UIViewController*)vc fromRect:(CGRect)rect fromVC:(UIViewController*)displayController{
+		
+	if (deviceIsPad){
+		if (_displayPopoper == nil){
+			_displayPopoper = [[UIPopoverController alloc] initWithContentViewController:vc];
+		}
+		
+		[_displayPopoper setContentViewController:vc];
+		[_displayPopoper presentPopoverFromRect:rect inView:displayController.view permittedArrowDirections:UIPopoverArrowDirectionDown animated:FALSE];
+	}else{
+		[displayController presentModalViewController:vc animated:TRUE];
+	}
+}
 
 #pragma mark swypContentDataSourceProtocol
 #pragma mark - delegation
@@ -150,6 +230,31 @@
 		abort();
 	} 
 		
+}
+
+#pragma mark - Delegation 
+#pragma mark UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+	UIImage * selectedImage	=	[info valueForKey:UIImagePickerControllerOriginalImage];
+	
+	if (_displayPopoper){
+		[_displayPopoper dismissPopoverAnimated:TRUE];
+	}else{
+		[self dismissModalViewControllerAnimated:TRUE];
+	}
+	
+	NSMutableDictionary *workingDictionary = [NSMutableDictionary dictionary];
+	[workingDictionary setObject:@"kUTTypeImage" forKey:@"UIImagePickerControllerMediaType"];
+	[workingDictionary setObject:selectedImage forKey:@"UIImagePickerControllerOriginalImage"];
+	
+		
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+	if (_displayPopoper){
+		[_displayPopoper dismissPopoverAnimated:TRUE];
+	}else{
+		[self dismissModalViewControllerAnimated:TRUE];
+	}
 }
 
 
